e3f640587197f0058a24696fca7412e4861ff57c
Jayme Nigri <jaymenigri@gmail.com>
2025-09-30 22:22:04 +0300

    Update twilioClient.js

--- services/twilioClient.js
+++ services/twilioClient.js
@@ -3,6 +3,10 @@ import { CONFIG } from "../config/appConfig.js";
 
 export const twilioClient = twilio(CONFIG.TWILIO.SID, CONFIG.TWILIO.TOKEN);
 
+/**
+ * Envia mensagem WhatsApp via Twilio.
+ * Faz log detalhado se falhar (motivo e código).
+ */
 export async function sendWhatsApp(to, body) {
   try {
     const r = await twilioClient.messages.create({
@@ -15,7 +19,9 @@ export async function sendWhatsApp(to, body) {
     const code = err?.code;
     const msg = err?.message || String(err);
     const more = err?.moreInfo || "";
-    console.error(`[TWILIO] send fail to=${to} from=${CONFIG.TWILIO.FROM} code=${code} msg=${msg} more=${more}`);
+    console.error(
+      `[TWILIO] send fail to=${to} from=${CONFIG.TWILIO.FROM} code=${code} msg=${msg} more=${more}`
+    );
     throw err;
   }
 }
