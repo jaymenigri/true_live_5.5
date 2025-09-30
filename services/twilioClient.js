8c9afd1ab72dd2b72c465f9709b6d9686a181842
Jayme Nigri <jaymenigri@gmail.com>
2025-09-30 11:42:14 +0300

    Update twilioClient.js

--- services/twilioClient.js
+++ services/twilioClient.js
@@ -1,7 +1,12 @@
-import Twilio from "twilio";
-const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHATSAPP_FROM } = process.env;
-export const twilioClient = Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+import twilio from "twilio";
+import { CONFIG } from "../config/appConfig.js";
+
+export const twilioClient = twilio(CONFIG.TWILIO.SID, CONFIG.TWILIO.TOKEN);
+
 export async function sendWhatsApp(to, body) {
-  if (!to?.startsWith("whatsapp:")) to = "whatsapp:" + to.replace(/^whatsapp:/, "");
-  return twilioClient.messages.create({ from: TWILIO_WHATSAPP_FROM, to, body });
+  return twilioClient.messages.create({
+    from: CONFIG.TWILIO.FROM,
+    to,
+    body
+  });
 }
