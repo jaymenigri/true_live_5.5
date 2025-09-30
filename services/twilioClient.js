fa4ff3c050043468c62abd95713f6f68273c2cd5
Jayme Nigri <jaymenigri@gmail.com>
2025-09-30 22:18:07 +0300

    Update twilioClient.js

--- services/twilioClient.js
+++ services/twilioClient.js
@@ -4,9 +4,18 @@ import { CONFIG } from "../config/appConfig.js";
 export const twilioClient = twilio(CONFIG.TWILIO.SID, CONFIG.TWILIO.TOKEN);
 
 export async function sendWhatsApp(to, body) {
-  return twilioClient.messages.create({
-    from: CONFIG.TWILIO.FROM,
-    to,
-    body
-  });
+  try {
+    const r = await twilioClient.messages.create({
+      from: CONFIG.TWILIO.FROM, // ex.: 'whatsapp:+14155238886' (sandbox) ou seu número aprovado
+      to,
+      body
+    });
+    return r;
+  } catch (err) {
+    const code = err?.code;
+    const msg = err?.message || String(err);
+    const more = err?.moreInfo || "";
+    console.error(`[TWILIO] send fail to=${to} from=${CONFIG.TWILIO.FROM} code=${code} msg=${msg} more=${more}`);
+    throw err;
+  }
 }
