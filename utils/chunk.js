4d68f021eaea9c92428d00d1ba707a80d66fef85
Jayme Nigri <jaymenigri@gmail.com>
2025-10-19 01:07:18 +0300

    Delete utils directory

--- utils/chunk.js
+++
@@ -1,9 +0,0 @@
-export function chunkMessage(text, maxLen = 1600) {
-  const parts = [];
-  let i = 0;
-  while (i < text.length) {
-    parts.push(text.slice(i, i + maxLen));
-    i += maxLen;
-  }
-  return parts;
-}
--- utils/lang.js
+++
@@ -1,8 +0,0 @@
-export function detectLang(s) {
-  if (!s) return "pt";
-  const t = s.toLowerCase();
-  if (/[א-ת]/.test(t)) return "he";
-  if (/(¿|¡)/.test(t) || /(ó|á|í|ñ)/.test(t)) return "es";
-  if (/[áéíóúãõç]/.test(t)) return "pt";
-  return "en";
-}
