81afa2b2bc7ddc62d4e7680048bfa8f41238a7fb
Jayme Nigri <jaymenigri@gmail.com>
2025-10-19 01:07:08 +0300

    Delete truelive-5.6-mvp directory

--- truelive-5.6-mvp/services/openaiClient.js
+++
@@ -1,6 +0,0 @@
-import OpenAI from "openai";
-import { CONFIG } from "../config/appConfig.js";
-export const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-export async function withTimeout(promise, ms = CONFIG.OPENAI_TIMEOUT_MS) {
-  return await Promise.race([ promise, new Promise((_, rej) => setTimeout(() => rej(new Error("OpenAI timeout")), ms)) ]);
-}
--- truelive-5.6-mvp/services/twilioClient.js
+++
@@ -1,6 +0,0 @@
-import twilio from "twilio";
-import { CONFIG } from "../config/appConfig.js";
-export const twilioClient = twilio(CONFIG.TWILIO.SID, CONFIG.TWILIO.TOKEN);
-export async function sendWhatsApp(to, body) {
-  return twilioClient.messages.create({ from: CONFIG.TWILIO.FROM, to, body });
-}
