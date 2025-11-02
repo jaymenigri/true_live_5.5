aaedb41d3af977053c54383b194cd3094047fb4d
Jayme Nigri <jaymenigri@gmail.com>
2025-11-02 14:57:11 +0200

    Refactor server.js to use express.json()

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { handleWhatsAppMessage } = require('./twilioHandler');
-const { pool } = require('./db');
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('True Live ativo.'));
+app.use(express.json());
 app.post('/twilio/whatsapp', handleWhatsAppMessage);
 
-pool.connect().then(() => console.info('[INFO] Contexto/PG pronto.'));
-app.listen(process.env.PORT || 3000, () => console.info('[INFO] Server ativo.'));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('[INFO] Server ativo.');
+});
